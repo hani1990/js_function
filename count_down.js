fcb9bfe227ded42de91f6ef7ebcca2f579560efc
Mac <mac@MacdeMacBook-Air.local>
2019-01-08 09:49:31 +0800

    fix

---
+++ count_down.js
@@ -0,0 +1,32 @@
+/*
+ * 倒计时
+*/
+function timer(intDiff) {
+        myTimer = window.setInterval(function () {
+            var day = 0,
+                hour = 0,
+                minute = 0,
+                second = 0;//时间默认值
+            if (intDiff > 0) {
+                day = Math.floor(intDiff / (60 * 60 * 24));
+                hour = Math.floor(intDiff / (60 * 60)) - (day * 24);
+                minute = Math.floor(intDiff / 60) - (day * 24 * 60) - (hour * 60);
+                second = Math.floor(intDiff) - (day * 24 * 60 * 60) - (hour * 60 * 60) - (minute * 60);
+            }
+            if (minute <= 9) minute = '0' + minute;
+            if (second <= 9) second = '0' + second;
+            $('#hour_show').html('<s id="h"></s>' + hour + '时');
+            $('#minute_show').html('<s></s>' + minute + '分');
+            $('#second_show').html('<s></s>' + second + '秒');
+            if (hour <= 0 && minute <= 0 && second <= 0) {
+                //time out todo something
+                clearInterval(myTimer);
+            }
+            intDiff--;
+
+            if (strcode != ""){
+                //checkdata();
+            }
+
+        }, 1000);
+    }
